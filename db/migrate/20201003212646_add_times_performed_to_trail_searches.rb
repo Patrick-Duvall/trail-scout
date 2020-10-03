6427a54fb8f68a7cac7e4826fe0d851c16ba2ce5
Patrick Duvall <duvallp208@gmail.com>
2020-10-03 16:09:38 -0600

    Remove times_performed

--- db/migrate/20201003212646_add_times_performed_to_trail_searches.rb
+++
@@ -1,5 +0,0 @@
-class AddTimesPerformedToTrailSearches < ActiveRecord::Migration[5.2]
-  def change
-    add_column :trail_searches, :times_performed, :integer, default: 1, null: false
-  end
-end
--- spec/services/trail_search_creator_spec.rb
+++ spec/services/trail_search_creator_spec.rb
@@ -20,37 +20,6 @@ RSpec.describe TrailSearchCreator do
       expect(trail_search.sort).to eq('distance')
       expect(trail_search.min_length).to eq(2)
       expect(trail_search.min_stars).to eq(3)
-      expect(trail_search.times_performed).to eq(1)
-    end
-
-    context 'when the same search is performed twice' do
-      before do
-        params_1 = { address: 'Denver, CO', maxResults: '1' }
-        params_2 = { address: 'Denver, CO', maxResults: '1' }
-        TrailSearchCreator.log_search(params_1)
-        TrailSearchCreator.log_search(params_2)
-      end
-
-      it 'will not create the same search twice' do
-        expect(TrailSearch.count).to eq(1)
-      end
-
-      it 'increases a searches count by one' do
-        expect(TrailSearch.last.times_performed).to eq(2)
-      end
-    end
-    
-    context 'when a search with some overlap is performed' do
-      before do
-        params_1 = {address: 'Denver, CO', maxResults: '1'}
-        params_2 = {address: 'Denver, CO', maxResults: '2'}
-        TrailSearchCreator.log_search(params_1)
-        TrailSearchCreator.log_search(params_2)
-      end
-
-      xit 'creates two different searches' do
-        
-      end
     end
   end
 end
