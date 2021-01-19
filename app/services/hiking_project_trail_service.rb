d43c2175407be06160abed897380756ea0ba95df
Patrick Duvall <duvallp208@gmail.com>
2021-01-19 08:47:08 -0700

    refactor hiking project trail service

--- app/services/hiking_project_trail_service.rb
+++ app/services/hiking_project_trail_service.rb
@@ -1,22 +1,19 @@
 class HikingProjectTrailService
 
-  def self.get_trails(params)
+  CLIENT =  Faraday.new("https://www.hikingproject.com") do |faraday|
+              faraday.adapter  Faraday.default_adapter
+            end
+
+  def self.get_trails(params, connection=CLIENT)
     new.get_trails(params.merge(key: ENV['HIKING_TRAILS_KEY']))
   end
 
   def get_trails(params)
-    trail_data = conn.get('data/get-trails') do |req|
+    trail_data = connection.get('data/get-trails') do |req|
         req.params = params
     end
     trails = JSON.parse(trail_data.body)['trails']
     return [] unless trails
     trails.map{|trail_details| Trail.new(trail_details)}
-  end
-
-private
-  def conn
-    Faraday.new("https://www.hikingproject.com") do |faraday|
-      faraday.adapter  Faraday.default_adapter
-    end
   end
 end
